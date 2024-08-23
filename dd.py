54e6bcfdd39d110c3d6a552a863a91443e14422e
zvkqO <149983232+zvkqO@users.noreply.github.com>
2024-08-23 19:31:17 +0900

    Delete dd.py

--- dd.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-HOST = "mongodb+srv://pinku03260707:<password>@kwargs.za1zguv.mongodb.net/"
-
-with pymongo.MongoClient(host = HOST) as client:
-    for db in client.list_databases():
-        print(db)
